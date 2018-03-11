146c52d4d9ef3a9d9d90c0a2ff195bcf04911b99
EC2 Default User <ec2-user@ip-172-31-13-104.us-east-2.compute.internal>
2018-03-11 00:00:25 +0000

    user login

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,5 +22,7 @@ module Travel
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ja
   end
 end
+
